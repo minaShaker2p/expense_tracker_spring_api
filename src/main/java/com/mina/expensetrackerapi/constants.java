abe7b3b62964dc2fffac7df492568018b13b9fc5
Mina Rezkalla <minashakeralfy@gmail.com>
2020-08-19 15:41:00 +0200

    create constants class with constants variables

---
+++ src/main/java/com/mina/expensetrackerapi/constants.java
@@ -0,0 +1,6 @@
+package com.mina.expensetrackerapi;
+
+public class constants {
+    public static final String API_SECRET_KEY = "expensetrackerapikey";
+    public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
+}
