e090452325caefb33dbadebf662cd7465ebd6e68
Mina Rezkalla <minashakeralfy@gmail.com>
2020-08-19 17:04:34 +0200

    add generate JWT token function when register and login

---
+++ src/main/java/com/mina/expensetrackerapi/Constants.java
@@ -0,0 +1,6 @@
+package com.mina.expensetrackerapi;
+
+public class Constants {
+    public static final String API_SECRET_KEY = "expensetrackerapikeyexpensetrackerapikeyexpensetrackerapikeyexpensetrackerapikeyexpensetrackerapikey";
+    public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
+}
--- src/main/java/com/mina/expensetrackerapi/controller/UserController.java
+++ src/main/java/com/mina/expensetrackerapi/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.mina.expensetrackerapi.controller;
 
+import com.mina.expensetrackerapi.Constants;
 import com.mina.expensetrackerapi.model.User;
 import com.mina.expensetrackerapi.service.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,15 +25,14 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<Map<String, String>> registerUser(@RequestBody Map<String, Object> userMap) {
+    public ResponseEntity registerUser(@RequestBody Map<String, Object> userMap) {
         String firstName = (String) userMap.get("firstName");
         String lastName = (String) userMap.get("lastName");
         String email = (String) userMap.get("email");
         String password = (String) userMap.get("password");
         User user = userService.registerUser(firstName, lastName, email, password);
         Map<String, String> result = new HashMap<>();
-        result.put("message", "registered successfully");
-        return new ResponseEntity(result, HttpStatus.OK);
+        return new ResponseEntity(generateJWTToken(user), HttpStatus.OK);
 
     }
 
@@ -38,8 +41,20 @@ public class UserController {
         String email = (String) usermap.get("email");
         String password = (String) usermap.get("password");
         User user = userService.validateUser(email, password);
+        return new ResponseEntity<>(generateJWTToken(user), HttpStatus.OK);
+    }
+
+    private Map<String, String> generateJWTToken(User user) {
+        long timeStamp = System.currentTimeMillis();
+        String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, Constants.API_SECRET_KEY)
+                .setIssuedAt(new Date(timeStamp))
+                .setExpiration(new Date(timeStamp + Constants.TOKEN_VALIDITY))
+                .claim("email", user.getEmail())
+                .claim("firstName", user.getFirstName())
+                .claim("lastName", user.getLastName())
+                .compact();
         Map<String, String> map = new HashMap<>();
-        map.put("message", "Logged Successfully");
-        return new ResponseEntity<>(map, HttpStatus.OK);
+        map.put("token", token);
+        return map;
     }
 }
